8a2d884dacfee87b9204ffb84b5653b90af88c87
apashintsev <av.pashintsev@gmail.com>
2025-12-24 22:01:20 +0300

    add send win script

---
+++ scripts/sendWin.ts
@@ -0,0 +1,21 @@
+import { Address, toNano } from '@ton/core';
+import { XPOK } from '../build/XPOK/XPOK_XPOK';
+import { NetworkProvider } from '@ton/blueprint';
+import { CONTRACTADDRESS } from './!YOURCONTRACTADDRESS';
+
+export async function run(provider: NetworkProvider) {
+    const contractAddress = Address.parse(CONTRACTADDRESS);
+
+    // Открываем существующий контракт по адресу
+    const superContract = provider.open(XPOK.fromAddress(contractAddress));
+
+    await superContract.send(
+        provider.sender(),
+        {
+            value: toNano('0.1'),
+        },
+        {
+            $$type: 'Win',
+        },
+    );
+}
